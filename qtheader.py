21aca0c17e0826dcf35926f077b800304acda589
Samuel Waugh <samuel.waugh@autodesk.com>
2017-11-25 13:21:35 -0500

    Initial commit

---
+++ qtheader.py
@@ -0,0 +1,18 @@
+from PySide.QtGui import QApplication, QWidget, QGridLayout, QPushButton, QPixmap, QLabel, QPalette, QSizePolicy, QTextEdit, QComboBox, QCheckBox, QLineEdit, QListWidget, QLayout, QIcon, QMessageBox, QDesktopWidget
+from PySide.QtCore import Qt, QRect
+from popup import popup
+import json_reader
+
+# This software is provided 'as-is'. Objeus and it's members are not responsible for any damages
+# caused by the use of this software.
+#
+# Unlimited distribution of this software is allowed. You may not modify or take credit for
+# any part of this software or the content that it produces.
+#
+# Persona is a registered trademark of ATLUS Inc. The images and text elements provided are
+# their sole possession. This software uses any copy-written material under the clause of 
+# fair use.
+#
+# Persona X Story Creator version 1.0.0.0
+# Author: Samuel Waugh (swwouf@hotmail.com)
+# All rights to their respective owners.
